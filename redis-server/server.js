0d1bd31e8212696e7b5843ba219b65794548b64d
Marca23 <141217300+Marca23@users.noreply.github.com>
2024-07-10 23:09:43 +0700

    update 3d model

--- redis-server/server.js
+++ redis-server/server.js
@@ -1,37 +1,49 @@
-const express = require('express');
-const redis = require('redis');
-const cors = require('cors');
+const express = require("express");
+const redis = require("redis");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const client = redis.createClient({
-    url: 'redis://10.10.10.84:6379' // Replace with your Redis server URL
+  url: "redis://10.10.10.84:6379", // Replace with your Redis server URL
+  socket: {
+    reconnectStrategy: function (retries) {
+      if (retries > 20) {
+        console.log(
+          "Too many attempts to reconnect. Redis connection was terminated"
+        );
+        return new Error("Too many retries.");
+      } else {
+        return retries * 500;
+      }
+    },
+  },
 });
 // http://localhost:3000/api/redis-data
 client.connect().catch(console.error);
 
-app.get('/api/redis-data', async (req, res) => {
-    try {
-        const idList = await client.lRange('id_list', 0, -1);
-        const data = {};
-        for (const key of idList) {
-            const value = await client.hGetAll(key);
-            if (Object.keys(value).length > 0) {
-                data[key] = Object.fromEntries(
-                    Object.entries(value).map(([k, v]) => {
-                        const parsedValue = parseFloat(v);
-                        return [k, k === 'x' ? parsedValue : parsedValue];
-                    })
-                );
-            }
-        }
-
-        res.json(data);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: 'An error occurred while fetching data' });
+app.get("/api/redis-data", async (req, res) => {
+  try {
+    const idList = await client.lRange("id_list", 0, -1);
+    const data = {};
+    for (const key of idList) {
+      const value = await client.hGetAll(key);
+      if (Object.keys(value).length > 0) {
+        data[key] = Object.fromEntries(
+          Object.entries(value).map(([k, v]) => {
+            const parsedValue = parseFloat(v);
+            return [k, k === "x" ? parsedValue : parsedValue];
+          })
+        );
+      }
     }
+
+    res.json(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
 });
 
 const PORT = 3000;
